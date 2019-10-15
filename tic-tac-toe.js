1a38eace20dd93949261e34fa88f8c2850405ccc
GarrikW <garrik.wong@gmail.com>
2019-10-15 00:48:17 -0500

    Javascript - added board layout to webpage

---
+++ tic-tac-toe.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    var i;
+	for (i = 3; i < 12; i++) {
+		document.getElementsByTagName("div")[i].setAttribute("class", "square");
+	}
+});
+
+
