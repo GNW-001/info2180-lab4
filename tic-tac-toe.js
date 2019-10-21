ab0fe46f3bdad9736bbe1a7174e3f37e31dcb89e
GNW-001 <55669962+GNW-001@users.noreply.github.com>
2019-10-21 14:02:35 -0500

    Added ability to place X/O markers

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,40 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    var i;
+	var i;
+	var i2 = 0;
+	var sheet = document.getElementsByTagName("div");
 	for (i = 3; i < 12; i++) {
-		document.getElementsByTagName("div")[i].setAttribute("class", "square");
+		sheet[i].setAttribute("class", "square");
+		sheet[i].setAttribute("id", "grid".concat(i2));
+		i2++;
 	}
+	addCellClickListener();
 });
 
+let board = ['', '', '', '', '', '', '', '', ''];
+let turn = 0;
 
+function addCellClickListener() {
+	var cells = document.getElementsByClassName("square");
+	for (var i3 = 0; i3 < cells.length; i3++) {
+		cells[i3].addEventListener("click", placeMark);
+	}
+}
+
+function placeMark() {
+	var temp = this.innerText;
+	if (temp == '') {
+		if (turn%2 == 0 ) {
+			this.innerHTML = "X";
+			this.classList.add("X");
+		}
+		else {
+			this.innerHTML = "O";
+			this.classList.add("O");
+		}
+		this.removeEventListener("click", placeMark);
+	}
+	else {
+		return;
+	}
+	turn ++;
+}
